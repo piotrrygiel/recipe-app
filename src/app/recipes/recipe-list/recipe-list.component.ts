49a831b84f74b2743c20fc57041bd5727a644d3c
ryglu <rygielpiotr18@gmail.com>
2024-02-28 15:09:04 +0100

    Create two services and start to manage recipes in a recipe service.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,17 +1,21 @@
-import {Component, EventEmitter, Output} from '@angular/core';
-import {Recipe} from "../recipe.model";
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import { Recipe } from "../recipe.model";
+import { RecipeService } from "../recipe.service";
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
-export class RecipeListComponent {
-  recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is a test recipe.', 'https://sashasfinefoods.com/cdn/shop/articles/WEB_Recipe_Images__700x500px_8.png?v=1658799349'),
-    new Recipe('Second recipe', 'This is a second recipe.', 'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/aadbcc1b4eff6d3d2a32361f1ad74c72/Derivates/755a4e73c9b36ccdaaa5886cd70948fae9ae7e03.jpg')
-  ];
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[];
   @Output() chosen_recipe = new EventEmitter<Recipe>();
+
+  constructor(private recipeService: RecipeService) {}
+
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
+  }
 
   onItemClicked(chosen_recipe: Recipe) {
     this.chosen_recipe.emit(chosen_recipe);
