d388010272559aab0036cbf34bd9fc3e2f92949e
ryglu <rygielpiotr18@gmail.com>
2024-02-29 23:28:49 +0100

    Add child routing and configure route parameters.

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,16 +1,27 @@
-import { Component, Input } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { Recipe } from "../recipe.model";
 import { RecipeService } from "../recipe.service";
+import {ActivatedRoute, Params} from "@angular/router";
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styleUrl: './recipe-detail.component.css'
 })
-export class RecipeDetailComponent {
-  @Input() recipe_detail: Recipe;
+export class RecipeDetailComponent implements OnInit {
+  recipe_detail: Recipe;
+  id: number;
 
-  constructor(private recipeService: RecipeService) {}
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.id = +params['id'];
+        this.recipe_detail = this.recipeService.getRecipe(this.id);
+      }
+    );
+  }
 
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe_detail.ingredients);
