9684b0c072bb66f4b341faac5d32334202820ecb
ryglu <rygielpiotr18@gmail.com>
2024-03-06 17:15:07 +0100

    Implement logic to fetch recipes.

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {RecipeService} from "../recipes/recipe.service";
+import {Recipe} from "../recipes/recipe.model";
 
 @Injectable({
   providedIn: 'root'
@@ -17,5 +18,13 @@ export class DataStorageService {
       .subscribe(response => {
         console.log(response);
       });
+  }
+
+  fetchRecipes() {
+    this.http
+      .get<Recipe[]>('https://ng-recipe-app-146db-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
+      .subscribe(recipes=> {
+        this.recipeService.setRecipes(recipes);
+      })
   }
 }
