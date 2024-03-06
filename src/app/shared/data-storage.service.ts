56528526dbd3fdd86101ef948760833600df4bdd
ryglu <rygielpiotr18@gmail.com>
2024-03-06 17:42:50 +0100

    Resolve data before loading.

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {RecipeService} from "../recipes/recipe.service";
 import {Recipe} from "../recipes/recipe.model";
-import {map} from "rxjs";
+import {map, tap} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -22,15 +22,16 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http
+    return this.http
       .get<Recipe[]>('https://ng-recipe-app-146db-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
       .pipe(map(recipes => {
         return recipes.map(recipe => {
           return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
         });
-      }))
-      .subscribe(recipes=> {
-        this.recipeService.setRecipes(recipes);
-      });
+      }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes);
+        })
+      );
   }
 }
