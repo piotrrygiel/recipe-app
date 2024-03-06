27461b46e2914200281ffcc7cf24080cd0578049
ryglu <rygielpiotr18@gmail.com>
2024-03-06 17:21:14 +0100

    Transform response data.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,24 +8,26 @@ import {Subject} from "rxjs";
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'A test recipe',
-      'This is a test recipe.',
-      'https://sashasfinefoods.com/cdn/shop/articles/WEB_Recipe_Images__700x500px_8.png?v=1658799349',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('Potato', 4)
-      ]),
-    new Recipe(
-      'Second recipe',
-      'This is a second recipe.',
-      'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/aadbcc1b4eff6d3d2a32361f1ad74c72/Derivates/755a4e73c9b36ccdaaa5886cd70948fae9ae7e03.jpg',
-      [
-        new Ingredient('Tomato sauce', 1),
-        new Ingredient('Mozzarella', 1)
-      ])
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'A test recipe',
+  //     'This is a test recipe.',
+  //     'https://sashasfinefoods.com/cdn/shop/articles/WEB_Recipe_Images__700x500px_8.png?v=1658799349',
+  //     [
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('Potato', 4)
+  //     ]),
+  //   new Recipe(
+  //     'Second recipe',
+  //     'This is a second recipe.',
+  //     'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/aadbcc1b4eff6d3d2a32361f1ad74c72/Derivates/755a4e73c9b36ccdaaa5886cd70948fae9ae7e03.jpg',
+  //     [
+  //       new Ingredient('Tomato sauce', 1),
+  //       new Ingredient('Mozzarella', 1)
+  //     ])
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {}
 
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {RecipeService} from "../recipes/recipe.service";
 import {Recipe} from "../recipes/recipe.model";
+import {map} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -23,8 +24,13 @@ export class DataStorageService {
   fetchRecipes() {
     this.http
       .get<Recipe[]>('https://ng-recipe-app-146db-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
+      .pipe(map(recipes => {
+        return recipes.map(recipe => {
+          return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+        });
+      }))
       .subscribe(recipes=> {
         this.recipeService.setRecipes(recipes);
-      })
+      });
   }
 }
