a7a703b6601facf22ff21089d1f0cc961816c0f5
ryglu <rygielpiotr18@gmail.com>
2024-03-09 16:14:38 +0100

    Implement core module (demo purposes) and add an auth feature module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,43 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from "./header/header.component";
 import { NgOptimizedImage } from "@angular/common";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { ShoppingListService } from "./shopping-list/shopping-list.service";
 import { AppRoutingModule } from "./app-routing.module";
-import {RecipeService} from "./recipes/recipe.service";
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {AuthComponent} from "./auth/auth.component";
-import {AuthInterceptorService} from "./auth/auth-interceptor.service";
+import {HttpClientModule} from "@angular/common/http";
 import {RecipesModule} from "./recipes/recipes.module";
 import {ShoppingListModule} from "./shopping-list/shopping-list.module";
 import {SharedModule} from "./shared/shared.module";
+import {CoreModule} from "./core.module";
+import {AuthModule} from "./auth/auth.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    AuthComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     NgOptimizedImage,
-    FormsModule,
     AppRoutingModule,
-    ReactiveFormsModule,
     HttpClientModule,
     RecipesModule,
     ShoppingListModule,
-    SharedModule
+    AuthModule,
+    SharedModule,
+    CoreModule
   ],
-  providers: [
-    ShoppingListService,
-    RecipeService,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptorService,
-      multi: true
-    }
-    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
